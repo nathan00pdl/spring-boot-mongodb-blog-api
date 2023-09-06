0c54bae4e1fe7c473405b8dc3c0e6004810cef5a
Nathan Paiva <nathanpaiva00@gmail.com>
2023-09-06 10:45:48 -0300

    insert user with method POST

--- src/main/java/com/MyExample/Projeto3_Java_Spring/services/UserService.java
+++ src/main/java/com/MyExample/Projeto3_Java_Spring/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.MyExample.Projeto3_Java_Spring.domain.User;
+import com.MyExample.Projeto3_Java_Spring.dto.UserDTO;
 import com.MyExample.Projeto3_Java_Spring.repositories.UserRepository;
 import com.MyExample.Projeto3_Java_Spring.services.exception.ObjectNotFoundException;
 
@@ -27,5 +28,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	//Inserindo Usuários
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	//Método que pega um DTO ('objDTO') e instancia um usuário
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
